3c10db31e6bc05e018dc644c19b49f018b4d46de
nanxinye <108196469+nanxinye@users.noreply.github.com>
2023-07-16 18:21:14 -0500

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,6 +5,7 @@ from translator import english_to_french,french_to_english
 class TestEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french("hello"),"bonjour")
+        self.assertNotEqual(english_to_french("hello"),"merci")
 
 class TestFrench(unittest.TestCase):
     def test1(self):
